d3104b0e39200c228ff866a23e3feda0fd2879df
UmarGit <umarsheikh303@gmail.com>
2020-04-09 23:03:25 +0500

    Update() path

--- src/main/java/project2/ReviewHandlerTest.java
+++ src/main/java/project2/ReviewHandlerTest.java
@@ -12,25 +12,26 @@ class ReviewHandlerTest {
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
-
+	// Your File Path to the Data folder !
+	private String path = "/Users/Power/eclipse-workspace/NEW/Data/";  
 	@Test
 	void testLoadReviews() {
 		ReviewHandler object1 = new ReviewHandler();
-		object1.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/positive-words.txt", 0);
-		object1.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/positive-words.txt", 1);
-		object1.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/positive-words.txt", 2);
+		object1.loadReviews(path+"positive-words.txt", 0);
+		object1.loadReviews(path+"positive-words.txt", 1);
+		object1.loadReviews(path+"positive-words.txt", 2);
 		ReviewHandler object2 = new ReviewHandler();
-		object2.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/negative-words.txt", 0);
-		object2.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/negative-words.txt", 1);
-		object2.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/negative-words.txt", 2);
+		object2.loadReviews(path+"negative-words.txt", 0);
+		object2.loadReviews(path+"negative-words.txt", 1);
+		object2.loadReviews(path+"negative-words.txt", 2);
 		ReviewHandler object3 = new ReviewHandler();
-		object3.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/neg", 0);
-		object3.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/neg", 1);
-		object3.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/neg", 2);
+		object3.loadReviews(path+"Movie-reviews/neg", 0);
+		object3.loadReviews(path+"Movie-reviews/neg", 1);
+		object3.loadReviews(path+"Movie-reviews/neg", 2);
 		ReviewHandler object4 = new ReviewHandler();
-		object4.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/pos", 0);
-		object4.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/pos", 1);
-		object4.loadReviews("/Users/Power/eclipse-workspace/NEW/Data/Movie-reviews/pos", 2);
+		object4.loadReviews(path+"Movie-reviews/pos", 0);
+		object4.loadReviews(path+"Movie-reviews/pos", 1);
+		object4.loadReviews(path+"Movie-reviews/pos", 2);
 	}
 
 //	@Test
